fc20bab7bde1a9a20d14f52dfea9e6ecc6133cdb
Rohith Asrk <rohith.asrk@gmail.com>
2017-02-11 16:11:38 +0530

    TodoApp: Feature: Save locally.

--- angular/todo_app/controller.js
+++ angular/todo_app/controller.js
@@ -1,7 +1,17 @@
 var appX = angular.module('todoApp', [])
 
 appX.controller('app', function($scope){
-    $scope.tasks = [];
+    var taskData = localStorage['tasksList'];
+
+    if(taskData != undefined){
+        $scope.tasks = JSON.parse(taskData);
+    }
+    else{
+        $scope.tasks = [];
+        console.log("Unable to parse");
+        console.log(taskData);
+    }
+    
     $scope.searchEnter = function(){
         if(event.which == 13 && $scope.task != ''){
             $scope.addTask();
@@ -12,15 +22,31 @@ appX.controller('app', function($scope){
         $scope.tasks.push({'taskMessage':$scope.task, 'status':false});
         console.log($scope.tasks);
         $scope.task = '';
+        localStorage['tasksList'] = JSON.stringify($scope.tasks);
+        console.log(localStorage);
     };
     
-    $scope.contentEdit = function(){  
+    $scope.contentEdit = function(msg){  
+        console.log($scope.tasks);
+
+        for(i=0;i<$scope.tasks.length;i++){
+            if($scope.tasks[i].taskMessage == msg){
+                $scope.tasks[i].taskMessage = event.target.innerText;
+            }
+        }
+        
+        localStorage['tasksList'] = JSON.stringify($scope.tasks);
+    
         event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false";
     };
     
     $scope.enterAgain = function(msg){
         if(event.which == 13 && msg != ''){
-            $scope.contentEdit();
+            $scope.contentEdit(msg);
         }
+    };
+    
+    $scope.statusChange = function(msg){
+        localStorage['tasksList'] = JSON.stringify($scope.tasks);
     };
 });
